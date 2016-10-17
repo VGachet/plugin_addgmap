ff6a0e510fc6bdcf4ba91771fb7f10ba28b33111
Vincent Gachet <vincent.gachet@eemi.com>
2016-10-17 21:12:50 +0200

    select gps locations + insert into js + generate latest gmap shortcode (to improve)

--- gmap.php
+++ gmap.php
@@ -36,3 +36,5 @@ function initializeMaps() {
     }
 }
 </script>
+
+<div id="map"></div>
--- plugin_gmap.php
+++ plugin_gmap.php
@@ -108,11 +108,11 @@ register_deactivation_hook( __FILE__, 'addgmap_drop_pins' );
 
 function plugin_setup_menu()
 {
-        add_menu_page( 'AddGmap', 'AddGmap', 'manage_options', 'plugin', 'my_plugin_options' );
+        add_menu_page( 'AddGmap', 'AddGmap', 'manage_options', 'plugin', 'insert_gmap_datas' );
 }
 add_action('admin_menu', 'plugin_setup_menu');
 
-function my_plugin_options() {
+function insert_gmap_datas() {
 	if ( !current_user_can( 'manage_options' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
@@ -121,11 +121,6 @@ function my_plugin_options() {
 
 	if(isset($_POST['name']))
 	{
-		foreach($_POST['lon'] as $longitude)
-		{
-			var_dump($longitude);
-		}
-		die;
 		$itineraire = false;
 		$allow_it = false;
 
@@ -141,54 +136,142 @@ function my_plugin_options() {
 
 		global $wpdb;
 
-		$table_name = $wpdb->prefix . 'addgmap';
+		/*Insert a new map into the addgmap_maps table*/
+		$table_name = $wpdb->prefix . 'addgmap_maps';
 
 		$data =	array(
-						'lat'			=>	$_POST['lat'],
-						'lon'			=>	$_POST['lon'],
-						'name'			=>	$_POST['name'],
-						'itineraire'	=>	$itineraire,
-						'allow_it'		=>	$allow_it
+						'allow_it'	=>	$allow_it
 				);
 		
 		$format = array(
-						'%s',
-						'%s',
-						'%s',
-						'%d',
 						'%d'
 				);
 
 		$wpdb->insert( $table_name, $data, $format );
-	}
-}
 
-function select_locations()
-{
-	global $wpdb;
+		/*Select the latest map_id inserted*/
+		$map_id_result = $wpdb->get_results( 
+			"
+			SELECT id_map 
+			FROM $table_name
+			ORDER BY id_map DESC
+			LIMIT 1
+			"
+ 		);
 
-	$results = $wpdb->get_results( 'SELECT * FROM wp_addgmap', OBJECT );
+ 		$map_id = $map_id_result[0]->id_map;
 
-	$count_locations = count($results);
+		/*Insert pins into the addgmap_pin table*/
 
-	$markers = "";
+		/*For each pin*/
+		$names = $_POST['name'];
+		$pin_number = 0;
 
-	$i = 1;
-
-	foreach ($results as $result) {
-
-		$name = "'" . $result->name . "'";
-
-		$markers .= "[" . $name . ",". $result->lat . "," . $result->lon . "]";
-		if($i < $count_locations)
+		foreach($names as $name)
 		{
-			$markers .= ",";
+			$table_name = $wpdb->prefix . 'addgmap_pin';
+
+			$lat = htmlspecialchars($_POST['lat'][$pin_number]);
+			$lon = htmlspecialchars($_POST['lon'][$pin_number]);
+
+			$data =	array(
+						'name'	=>	$name,
+						'lat'	=>	$lat,
+						'lon'	=>	$lon
+				);
+		
+			$format = array(
+							'%s',
+							'%s',
+							'%s'
+					);
+
+			$wpdb->insert( $table_name, $data, $format );
+
+			$pin_number++;
 		}
-		$i++;
+
+		/*Select latest pins inserted*/
+		$pin_id_results = $wpdb->get_results( 
+			"
+			SELECT id_pin 
+			FROM $table_name
+			ORDER BY id_pin DESC
+			LIMIT $pin_number;
+			"
+ 		);
+
+ 		foreach ($pin_id_results as $pin_id) {
+ 			/*Insert the new map id for each new pins in the addgmap_pins table*/
+			$table_name = $wpdb->prefix . 'addgmap_pins';
+
+			$data =	array(
+						'id_pins_pin'	=>	$pin_id->id_pin,
+						'id_pins_map'	=>	$map_id
+				);
+		
+			$format = array(
+							'%d',
+							'%d'
+					);
+
+			$wpdb->insert( $table_name, $data, $format );
+ 		}
 	}
-	require('gmap.php');
 }
-add_action('plugins_loaded', 'select_locations');
+
+function generate_latest_gmap_shortcode()
+{
+		global $wpdb;
+
+		/*Select latest map inserted*/
+
+		$table_name = $wpdb->prefix . 'addgmap_pins';
+
+		$latest_map_results = $wpdb->get_results( 
+			"
+			SELECT * 
+			FROM $table_name
+			WHERE id_pins_map = ( SELECT MAX(id_pins_map) FROM $table_name );
+			"
+ 		);
+
+ 		$latest_map_id = $latest_map_results[0]->id_pins_map;
+
+ 		/*Count the number of pins*/
+ 		$count_pins = count($latest_map_results);
+
+ 		/*Initialize the marker variable => included into the gmap js later*/
+
+ 		 $markers = "";
+
+ 		 $i = 1;
+
+ 		foreach($latest_map_results as $latest_result)
+ 		{
+ 			$table_name = $wpdb->prefix . 'addgmap_pin';
+
+ 			$latest_pin_results = $wpdb->get_results( 
+				"
+				SELECT *
+				FROM $table_name AS pin
+				WHERE pin.id_pin = $latest_result->id_pins_pin;
+				"
+ 			);
+
+ 			/*add pin name, pin lat and pin long to the markers var*/
+
+ 			$markers .= "['" . $latest_pin_results[0]->name . "',". $latest_pin_results[0]->lat . "," . $latest_pin_results[0]->lon . "]";
+ 			if($i < $count_pins)
+			{
+				$markers .= ",";
+			}
+			$i++;
+ 		}
+
+ 		require('gmap.php');
+}
+add_shortcode( 'addgmap', 'generate_latest_gmap_shortcode' );
 
 function gmap_enqueue_script()
 {
